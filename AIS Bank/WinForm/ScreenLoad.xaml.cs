9062469dfd012cc7a92ed3c79e88bfb2fab5b910
ivan.tsovdoshin@yandex.ru <ivan.tsovdoshin@yandex.ru>
2021-04-20 21:53:24 +0500

    Добавьте файлы проекта.

---
+++ AIS Bank/WinForm/ScreenLoad.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AIS_Bank
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 1);
+            dt.Start();
+        }
+
+        int startpoint = 0;
+
+        private void dt_Tick(object sender, EventArgs e)
+        {
+            startpoint += 15;
+            MyprogressBar.Value = startpoint;
+            ProgresBar.Content = startpoint + "%";
+            if (MyprogressBar.Value == 100)
+            {
+                MyprogressBar.Value = 0;
+                dt.Stop();
+                Login LG = new Login();
+                LG.Show();
+                this.Close();
+            }
+        }
+   
+    }
+}
